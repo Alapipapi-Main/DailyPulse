9bb6fac6aa2a0aee40e97caa9c7f56f3208b02c9
Muhammad Imran Ismail <imranismail0610@gmail.com>
2026-01-20 09:03:55 +0000

    Now make the favicon icon looks better and same as the website icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%236699CC' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3e%3cpath d='M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2h0a2 2 0 0 0 0 4h.5Z'/%3e%3cpath d='M4 22V4'/%3e%3cpath d='M12 14h4'/%3e%3cpath d='M12 18h4'/%3e%3cpath d='M12 10h4'/%3e%3c/svg%3e" />
+        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3e%3cpath fill='%236699CC' d='M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2h0a2 2 0 0 0 0 4h.5Z'/%3e%3cpath stroke='%23F5F7FA' stroke-width='2' stroke-linecap='round' d='M4 22V4'/%3e%3cpath stroke='%23F5F7FA' stroke-width='2' stroke-linecap='round' d='M12 14h4'/%3e%3cpath stroke='%23F5F7FA' stroke-width='2' stroke-linecap='round' d='M12 18h4'/%3e%3cpath stroke='%23F5F7FA' stroke-width='2' stroke-linecap='round' d='M12 10h4'/%3e%3c/svg%3e" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
